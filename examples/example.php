31697d00c5c9fc659297ad4a27da439ca914b789
klinson <klinson@163.com>
2019-05-16 10:16:06 +0800

    实现

---
+++ examples/example.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: klinson <klinson@163.com>
+ * Date: 19-5-15
+ * Time: 下午5:35
+ */
+
+require __DIR__.'/../src/XiaoIce.php';
+
+use MsXiaoIce\XiaoIce;
+
+function getOriginImages($saveName)
+{
+    $path = __DIR__ . '/originImages/' . $saveName;
+    return $path;
+}
+
+function saveProcessedImages($url, $saveName)
+{
+    $path = __DIR__ . '/processedImages/' . $saveName;
+    file_put_contents($path, file_get_contents($url));
+}
+
+//$img = 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1557905228&di=eed5cbd0ccddca5040f6d210bc58627b&src=http://b-ssl.duitang.com/uploads/item/201512/12/20151212193107_ujGZV.jpeg';
+// 颜值鉴定 / appraise appearance score
+$img = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557915647241&di=4248f55beae825b50491aca53512bd2a&imgtype=0&src=http%3A%2F%2Fimg.tukexw.com%2Fimg%2F8e675ce41005d9fc.jpg';
+$res = XiaoIce::getInstance()->appraiseScore($img);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img1-1.jpeg');
+$path = getOriginImages('img1.jpg');
+$res = XiaoIce::getInstance()->appraiseScore($path);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img1-2.jpeg');
+
+
+// 拼颜值 / compete appearance
+$img ='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557916418774&di=a79bd9eef9d1e64f890badc9fb49b5c9&imgtype=0&src=http%3A%2F%2Fp0.ifengimg.com%2Fpmop%2F2018%2F0705%2F27F3EA7EC281BDF657F322A1B8098D44638FD1C5_size127_w960_h1128.jpeg';
+$res = XiaoIce::getInstance()->competeAppearance($img);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img2-1.jpeg');
+$path = getOriginImages('img2.jpeg');
+$res = XiaoIce::getInstance()->competeAppearance($path);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img2-2.jpeg');
+
+// 测CP / speculate CP
+$img = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557981569487&di=3558d3fb26cf89263549edd707ddd1c3&imgtype=0&src=http%3A%2F%2Fimg.tuohuangzu.com%2Fthz%2Fuserblog%2F0%2F13%2F2014030415073246634.png';
+$res = XiaoIce::getInstance()->speculateCP($img);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img3-1.jpeg');
+$path = getOriginImages('img3.jpeg');
+$res = XiaoIce::getInstance()->speculateCP($path);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img3-2.jpeg');
+
+// 谁请客 / who treat
+$img = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557916418774&di=a79bd9eef9d1e64f890badc9fb49b5c9&imgtype=0&src=http%3A%2F%2Fp0.ifengimg.com%2Fpmop%2F2018%2F0705%2F27F3EA7EC281BDF657F322A1B8098D44638FD1C5_size127_w960_h1128.jpeg';
+$res = XiaoIce::getInstance()->whoTreat($img);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img4-1.jpeg');
+$path = getOriginImages('img4.jpeg');
+$res = XiaoIce::getInstance()->whoTreat($path);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img4-2.jpeg');
+
+// 测年龄 / speculate age
+$img = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557915647241&di=4248f55beae825b50491aca53512bd2a&imgtype=0&src=http%3A%2F%2Fimg.tukexw.com%2Fimg%2F8e675ce41005d9fc.jpg';
+$res = XiaoIce::getInstance()->speculateAge($img);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img5-1.jpeg');
+$path = getOriginImages('img5.jpg');
+$res = XiaoIce::getInstance()->speculateAge($path);
+var_dump($res);
+saveProcessedImages($res['image_url'], 'img5-2.jpeg');
---
+++ src/XiaoIce.php
@@ -0,0 +1,276 @@
+<?php
+namespace MsXiaoIce;
+
+use Exception;
+use BadMethodCallException;
+
+/**
+ * 微软小冰颜值测试
+ * Class XiaoIce
+ * @method boolean|array appraiseScore($image, $isBase64 = false) 颜值鉴定
+ * @method boolean|array competeAppearance($image, $isBase64 = false) 拼颜值
+ * @method boolean|array speculateCP($image, $isBase64 = false) 测CP
+ * @method boolean|array whoTreat($image, $isBase64 = false) 谁请客
+ * @method boolean|array appraiseDress($image, $isBase64 = false) 时尚穿衣秘籍
+ * @method boolean|array speculateAge($image, $isBase64 = false) 测年龄
+ * @param string $image 文件url或者本地地址或者文件base64编码
+ * @package MsXiaoIce
+ * @author klinson <klinson@163.com>
+ * @link https://www.klinson.com
+ */
+class XiaoIce
+{
+    // 访问host
+    protected $host = 'kan.msxiaobing.com';
+    // 启动https？
+    protected $isHttps = true;
+
+    protected $urls = [
+        'appraiseScore' => [
+            'page' => '/ImageGame/Portal?task=beauty',
+            'api' => '/Api/ImageAnalyze/Process?service=beauty',
+        ],
+        'competeAppearance' => [
+            'page' => '/ImageGame/Portal?task=yanzhi',
+            'api' => '/Api/ImageAnalyze/Process?service=yanzhi',
+        ],
+        'speculateCP' => [
+            'page' => '/ImageGame/Portal?task=guanxi',
+            'api' => '/Api/ImageAnalyze/Process?service=guanxi',
+        ],
+        'whoTreat' => [
+            'page' => '/ImageGame/Portal?task=qingke',
+            'api' => '/Api/ImageAnalyze/Process?service=qingke',
+        ],
+        'speculateAge' => [
+            'page' => '/ImageGame/Portal?task=howold',
+            'api' => '/Api/ImageAnalyze/Process?service=howold',
+        ],
+    ];
+    // 上传图片接口
+    protected $fileApi = '/Api/Image/UploadBase64';
+
+    // 保存会话的文件
+    protected $cookieFile = '';
+
+    protected static $instance;
+    protected function __construct()
+    {
+        // 建立一个零时文件
+        $this->cookieFile = tempnam('./temp', 'cookie');
+    }
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * 验证服务主页是否正常可访问
+     * @throws Exception
+     * @author klinson <klinson@163.com>
+     */
+    private function check($api)
+    {
+        $url = $this->getUrl($api, 0);
+        $ret = $this->curl($url);
+        if (empty($ret)) {
+            $this->setError('访问颜值测试主页失败');
+        }
+    }
+
+    /**
+     * 接口统一调用
+     * @param string $api 请求接口
+     * @param string $image 本地或网络的文件路径或者base64编码内容
+     * @param boolean $isBase64 传入的$image是否是base64编码内容
+     * @throws Exception
+     * @return boolean|array 测试是否成功
+     */
+    protected function handle($api, $image, $isBase64 = false)
+    {
+//        $api = __FUNCTION__;
+        $this->check($api);
+
+        $ret = $this->uploadImage($image, $isBase64);
+        if (!isset($ret->Host) || !isset($ret->Host)) {
+            return $this->setError('上传图片到微软服务器错误');
+        }
+
+        $data = [
+            'MsgId' => str_pad(time(), 13, '0'),
+            'CreateTime' => time(),
+            'Content[imageUrl]' => $ret->Host . $ret->Url,
+        ];
+
+        $url = $this->getUrl($api, 1);
+        $rsp = $this->curl($url, $data, 'POST', $this->getCurlHeaders($api));
+        if (empty($rsp)) {
+            return $this->setError('测试失败');
+        }
+
+        $response = json_decode($rsp, true);
+
+        if (!isset($response['content'])) {
+            return $this->setError('返回的数据异常');
+        }
+
+        if (isset($response['content']['metadata']['score']) && $response['content']['metadata']['score'] > 0) {
+            $score = $response['content']['metadata']['score'];
+        } else {
+            preg_match('/[-+]?([0-9]*\.[0-9]+|[0-9]+)/', $response['content']['text'], $matches);
+            if (!empty($matches) && isset($matches[0])) {
+                $score = $matches[0];
+            } else {
+                $score = 0;
+            }
+        }
+
+        return [
+            'result' => $score,
+            'text' => $response['content']['text'],
+            'image_url' => $response['content']['imageUrl'],
+        ];
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (key_exists($name, $this->urls)) {
+            return $this->handle($name, $arguments[0]);
+        } else {
+            throw new BadMethodCallException('Call to undefined method ' . __CLASS__ . '::' . $name . '()');
+        }
+    }
+
+    /**
+     * 设置错误信息
+     * @param string $message
+     * @author klinson <klinson@163.com>
+     * @throws Exception
+     */
+    private function setError($message = '')
+    {
+        $this->error = $message;
+        throw new \Exception($message);
+    }
+
+    /**
+     * 上传图片
+     * @param  string $file 本地或网络地址
+     * @param boolean $isBase64 传入的$image是否是base64编码内容
+     * @return boolean|string|array       响应数据
+     */
+    private function uploadImage($file, $isBase64 = false)
+    {
+        if ($isBase64) {
+            $img_data = $file;
+        } else {
+            $img_data = $this->getBase64DataByFilePath($file);
+        }
+
+        $url = ($this->isHttps ? 'https://' : 'http://') . $this->host . $this->fileApi;
+        $rsp = $this->curl($url, $img_data, 'post', [], true);
+        if (empty($rsp)) {
+            return false;
+        }
+        return json_decode($rsp);
+    }
+
+    /**
+     * 统一curl头
+     * @author klinson <klinson@163.com>
+     * @return array
+     */
+    private function getCurlHeaders($api)
+    {
+        $headers = [
+            'Referer' => $this->getUrl($api, 0),
+            'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+            'Connection' => 'close',
+            'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Host' => $this->host,
+            'Origin' => ($this->isHttps ? 'https://' : 'http://') . $this->host
+        ];
+
+        return $headers;
+    }
+
+    /**
+     * 发出请求
+     * @param  string           $url     URL
+     * @param  array|string     $request 请求参数
+     * @param  string           $method  请求方法
+     * @return string
+     */
+    private function curl($url, $request = null, $method = 'GET', $headers = [], $is_binary = false)
+    {
+        $ch = curl_init();
+        $method = strtoupper($method);
+        if ($method == 'GET') {
+            if (is_array($request)) {
+                $url = $url . '?' . http_build_query($request);
+            }
+        }
+        if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            if ($is_binary) {
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+            } else {
+                if (is_array($request)) {
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($request));
+                }
+            }
+        }
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->paramHeader($headers));
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // cookie文件
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return $res;
+    }
+
+    private function getUrl($apiName, $isApi = 1)
+    {
+        return ($this->isHttps ? 'https://' : 'http://') . $this->host . $this->urls[$apiName][($isApi ? 'api' : 'page')];
+    }
+
+    /**
+     * 解析header
+     * @param array $headers
+     * @return array 解析后的头部数组
+     */
+    private function paramHeader($headers)
+    {
+        $arr = [];
+        foreach ($headers as $key => $value) {
+            $arr[] = $key . ':' . $value;
+        }
+        return $arr;
+    }
+
+    /**
+     * 获取图片的Base64编码的数据
+     * @param  string $file_path 本地或网络地址
+     * @return string
+     */
+    private function getBase64DataByFilePath($file_path)
+    {
+        return base64_encode(file_get_contents($file_path));
+    }
+}
+
+
+
